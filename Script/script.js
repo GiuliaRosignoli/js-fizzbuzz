5c4f47aabc288277c1f0bd0086a15b182c35e10e
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-03-23 15:07:57 +0100

    .

---
+++ Script/script.js
@@ -0,0 +1,9 @@
+/**    TASK
+ 
+* Generate a list of numbers from 1 to 100
+* If they're multiple of 3 ---> Fizz
+* If they're multiple of 5 ----> Buzz
+* If they're multiple of both of them ---> Fizz Buzz
+*/
+
+
