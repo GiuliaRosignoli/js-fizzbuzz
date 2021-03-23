c5976bb1d36d1445f5e09cde3b2275ff722bdc61
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-03-23 15:19:47 +0100

    Print numbers from 1 to 100

--- Script/script.js
+++ Script/script.js
@@ -1,9 +1,20 @@
 /**    TASK
  
 * Generate a list of numbers from 1 to 100
-* If they're multiple of 3 ---> Fizz
-* If they're multiple of 5 ----> Buzz
-* If they're multiple of both of them ---> Fizz Buzz
+* If they're multiple of 3 ---> Print Fizz
+* If they're multiple of 5 ----> Print Buzz
+* If they're multiple of both of them ---> Print Fizz Buzz
 */
 
+// List of numbers from 1 to 100 to be printed
+
+var numbers = new Array (100);
+
+for (var i = 0; i < 100; i++) {
+    numbers[i] = i +  1;   // Populate the array
+}
+
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]); // Print all the values stored in the array
+}
 
